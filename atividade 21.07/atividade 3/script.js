97ea896e0324e3d26b4bcb3ede45dd2fd0623eb7
jeffersonsjsilva <jeffe.santana19@gmail.com>
2023-07-21 17:19:20 -0300

    Adição de aula sobre Array

---
+++ atividade 21.07/atividade 3/script.js
@@ -0,0 +1,8 @@
+let arr = ["Manoel", "Baptista", "Alfandega", "China"," Limão"]
+arr[2] = "Aeroporto"
+arr[4] = "Laranja"
+arr.push("Banana");
+
+let arra = ["Luffy", "Zoro"]
+
+console.log(arr.concat(arra))
