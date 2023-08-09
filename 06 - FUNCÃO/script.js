6099c3cc03532ac1842946aca0aa2103eaac35a3
jeffersonsjsilva <jeffe.santana19@gmail.com>
2023-08-09 17:24:20 -0300

    FUNÇÃO

---
+++ 06 - FUNCÃO/script.js
@@ -0,0 +1,49 @@
+// Exemplo 01
+
+// function imprimirNoConsole(){
+// console.log("Olá Jovem")
+// }
+// imprimirNoConsole()
+
+// Exemplo 02
+
+// function imprimirNumero(num){
+//     console.log(`O número é: ${num}`)
+// }
+// imprimirNumero(15);
+
+// Desafio 01 - imprimir no console o nome do usuario utilizando função e prompt
+
+// function nomeDoUsuario (nome){
+//     console.log(`O seu nome é ${nome}`)
+// }
+
+// nomeDoUsuario(prompt("Digite seu nome"));
+
+// // Desafio 02 - imprimir o dobro do número usando função
+
+// function imprimirDobro(num){
+//  num *= 2
+//  console.log(num)
+// }
+// imprimirDobro(prompt("Digite um número"))
+
+// Desafio 03 - usar função para imprimir a potencia de um numero por ele mesmo
+
+// function potencia(num){
+// var potencia = Math.pow(num,num)
+// console.log(`O número ${num} elevado a ele mesmo é ${potencia}`)
+// }
+// potencia(prompt("Digite um número"))
+
+// desafio 04 = fazer uma função que multiplica 3 números
+
+// function multiplicar (a, b,c){
+//     let num = a*b*c
+//     console.log(num)
+// }
+
+// multiplicar(10,7,9)
+
+
+//
--- RECUPERAÇÃO/script.js
+++ RECUPERAÇÃO/script.js
@@ -36,10 +36,73 @@ crescente usando o método sort(). */
 
 
 
-const n = Number(prompt("Quantos números você irá digitar?"))
-for(let x = 0; x < n; x++){
-    let arr = [Number(prompt("Digite os números separados por virgula exemplo: 1,2,3,4,5"))]
-    console.log(arr)
-}
+// const n = Number(prompt("Quantos números você irá digitar?"))
+// for(let x = 0; x < n; x++){
+//     let arr = [Number(prompt("Digite os números separados por virgula exemplo: 1,2,3,4,5"))]
+//     console.log(arr)
+// }
 // let soma = (a,b) => a+b && a/b
 // const total = arr.reduce(soma,0)
+
+/* 01 -criar um programa que solicita ao usuário que insira seu nome usando o comando prompt() 
+e exibe uma mensagem de boas-vindas usando o comando alert().*/
+// let nome = prompt("Digite seu nome")
+// alert(`Seja Bem-indo ${nome}`)
+
+/* 02 - Exemplo: criar um programa que verifica se um número é par ou ímpar usando o 
+operador de resto e o operador de igualdade.*/
+
+// let n = Number(prompt("Digite um número"))
+// if(n % 2 === 0){
+//     alert(`O número ${n} é par`)
+// }else{
+//     alert(`O número ${n} é impar`)
+// }
+
+/*03 - Exemplo: criar um programa que percorre um vetor de 
+números usando a estrutura de repetição for e exibe a soma 
+dos números usando o método reduce(). */
+
+// const arr = [1,2,3,4,5]
+// let soma = (a,b) => a+b
+
+// // for(let x = 0; x < arr.length; x++){
+// // soma += arr[x]
+// // }
+//USANDO O REDUCE()
+// const total = arr.reduce(soma,0)
+// console.log(total)
+
+/* 04 - Exemplo: criar um programa que solicita ao usuário que insira 
+uma lista de números separados por vírgulas usando o comando prompt() 
+e exibe a soma e a média dos números usando métodos de vetor em JavaScript, 
+como split() e reduce(). Em seguida, exibe os números ordenados em ordem 
+crescente usando o método sort(). */
+
+// const n = Number(prompt("Quantos números você irá digitar?"))
+// let arr = []
+// for(let x = 0; x < n; x++){
+//     let num = [Number(prompt("Digite os números separados por virgula exemplo: 1,2,3,4,5"))]
+//     arr = num.concat(arr)
+// }
+// console.log(arr)
+// let soma = (a,b) => a+b
+// const total = arr.reduce(soma,0)
+// console.log(total)
+// const divisao = n/ arr.length
+// console.log(divisao)
+// console.log(arr.sort())
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0,"Limao","manga")
+// console.log(fruits)
+
+
+// let n = prompt("Digite os números separados por virgula")
+// arr =n.split(",")
+
+// console.log (arr)
+
+let n = 15
+
+console.log(n & 9)
